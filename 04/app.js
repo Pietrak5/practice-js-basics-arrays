5fab4431b83655859647a1a4cba09fc4482609f6
Piotr <you@example.com>
2023-08-27 11:44:04 +0200

    add task 3

--- 04/app.js
+++ 04/app.js
@@ -1,1 +1,9 @@
-const years = [1980, 1934, 2002, 2019];
+const years = [1980, 1934, 2002, 2019]
+const date = new Date()
+let year = date.getFullYear()
+
+function newYear(x) {
+	return year - x
+}
+const newArrey = years.map(newYear)
+console.log(newArrey)
